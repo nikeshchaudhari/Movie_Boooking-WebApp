e93576571e884392291b7df7b985ee3284d9add3
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-28 21:38:48 +0545

    frontend

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
           <label htmlFor="email" className="text-white mb-2 font-semibold ">Email</label>
           <input type="text" placeholder="e.g.example@gmail.com " className="border  w-[400px]  h-10  bg-white rounded-lg p-4 focus:border-red-500 focus:outline-none mb-6"/>
           <label htmlFor="password" className="text-white mb-2 font-semibold ">Password</label>
-           <input type="text" placeholder="e.g.example@gmail.com " className="border  w-[400px]  h-10  bg-white rounded-lg p-4 focus:border-red-500 focus:outline-none mb-6"/>
+           <input type="text" placeholder="*********** " className="border  w-[400px]  h-10  bg-white rounded-lg p-4 focus:border-red-500 focus:outline-none mb-6"/>
 
            <button type="submit" className=" bg-red-600 w-[400px] h-10 rounded-4xl text-white font-bold cursor-pointer hover:bg-red-900 transition hover:duration-500 mb-4">Login</button>
            <p className="text-center w-full text-white">Don’t have an account? <span className="text-[#00E5FF]">Register</span></p>
