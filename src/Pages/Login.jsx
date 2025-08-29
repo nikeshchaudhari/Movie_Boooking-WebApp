64188784fc9cec8db0d4081b40411d3a8fd60830
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-29 23:38:19 +0545

    frontend

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+const initialValues={
+  email :"",
+  password: "",
+};
 const Login = () => {
   return (
     <>
