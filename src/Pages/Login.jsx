40d2497b6ca2037b2749535e36fdc0bd30966596
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-30 21:39:22 +0545

    frontend

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -13,11 +13,13 @@ const[password,setPassword]= useState("")
     e.preventDefault();
     
 
-     if(email==="test@gmail.com"&& passsword ==="12345"){
+     if(email==="test@gmail.com"&& password ==="12345"){
       dispatch(login({
         user:{email:email},
-        passsword :{passsword:passsword}
+        token:"dummy1234"
       }))
+      console.log("Login sucessfull");
+      
      }else{
       console.log("NOt valid");
       
@@ -35,7 +37,7 @@ const[password,setPassword]= useState("")
           <label htmlFor="email" className="text-white mb-2 font-semibold ">Email</label>
           <input type="text" placeholder="e.g.example@gmail.com " className="border  w-[400px]  h-10  bg-white rounded-lg p-4 focus:border-red-500 focus:outline-none mb-6" value={email} onChange={(e)=>setEmail(e.target.value)}/>
           <label htmlFor="password" className="text-white mb-2 font-semibold ">Password</label>
-           <input type="text" placeholder="*********** " className="border  w-[400px]  h-10  bg-white rounded-lg p-4 focus:border-red-500 focus:outline-none mb-6" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+           <input type="password" placeholder="*********** " className="border  w-[400px]  h-10  bg-white rounded-lg p-4 focus:border-red-500 focus:outline-none mb-6" value={password} onChange={(e)=>setPassword(e.target.value)}/>
 
            <button type="submit" className=" bg-red-600 w-[400px] h-10 rounded-4xl text-white font-bold cursor-pointer hover:bg-red-900 transition hover:duration-500 mb-4">Login</button>
            <p className="text-center w-full text-white">Don’t have an account? <span className="text-[#00E5FF]">Register</span></p>
