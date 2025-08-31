e9060ff0c98a1fe32437d5129e93b2a0a5e74902
nikesh chaudhari <info.nikesh12@gmail.com>
2025-08-31 23:25:55 +0545

    frontend

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
+import OnShowing from '../components/Onshowing'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
     
       <Navbar/>
        <Slider/>
+       <OnShowing/> 
   
     
     </>
