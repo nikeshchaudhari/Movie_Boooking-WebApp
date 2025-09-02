0ebf0399c86ce7a84621419429895d4ee555e04e
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-03 00:44:13 +0545

    frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,10 @@ const Navbar = () => {
   return (
     <>
       <div className="relative">
-        <div className=" bg-black/20 md:w-full h-20 flex  justify-around items-center ">
+        <div className=" bg-white md:w-full h-20 flex  justify-around items-center ">
           <div className="w-[80vw] md:w-[20vw] mx-5 md:mx-0">
             <NavLink to="/home">
-              <h1 className="text-white  text-[20px] lg:text-[30px] mx-2 lg:ml-3 md:font-semibold  md:w-[300px]">
+              <h1 className="text-black  text-[20px] lg:text-[30px] mx-2 lg:ml-3 md:font-semibold  md:w-[300px]">
                 MOVIES CINEMA{" "}
               </h1>
             </NavLink>
@@ -35,7 +35,7 @@ const Navbar = () => {
                   className={({ isActive }) => {
                     return isActive || window.location.pathname === "/"
                       ? "text-yellow-500"
-                      : "text-white";
+                      : "text-black";
                   }}
                 >
                   Home
@@ -45,7 +45,7 @@ const Navbar = () => {
                 <NavLink
                   to="/movies"
                   className={({ isActive }) => {
-                    return isActive ? "text-yellow-500" : "text-white";
+                    return isActive ? "text-yellow-500" : "text-black";
                   }}
                 >
                   Movie
@@ -55,7 +55,7 @@ const Navbar = () => {
                 <NavLink
                   to="/ticket-rate"
                   className={({ isActive }) => {
-                    return isActive ? "text-yellow-500" : "text-white";
+                    return isActive ? "text-yellow-500" : "text-black";
                   }}
                 >
                   Ticket Rate
@@ -65,10 +65,10 @@ const Navbar = () => {
           </div>
           {isLoggin ? (
             <>
-              <span className="text-white">{user.email}</span>
+              <span className="text-black ">{user.email}</span>
               <button
                 onClick={() => dispatch(logout())}
-                className="transition duration-200  hover:text-yellow-500 cursor-pointer font-medium text-white"
+                className="transition duration-200  hover:text-yellow-500 cursor-pointer font-medium text-black"
               >
                 Logout
               </button>
@@ -97,9 +97,9 @@ const Navbar = () => {
 
           <div className="mr-5 md:hidden" onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? (
-              <MdClose className="text-white text-[25px]" />
+              <MdClose className="text-black text-[25px]" />
             ) : (
-              <RxHamburgerMenu className="text-white text-[25px]" />
+              <RxHamburgerMenu className="text-black text-[25px]" />
             )}
           </div>
         </div>
