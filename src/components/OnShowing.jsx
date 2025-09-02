0f83d49a44b0ba6f95d4ae9856266dd4d40b972e
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-02 17:59:45 +0545

    frontend

--- src/components/OnShowing.jsx
+++ src/components/OnShowing.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setMovies,setSelectDate } from "../feature/movie/movieSlice";
+import { setMovies, setSelectDate } from "../feature/movie/movieSlice";
 import axios from "axios";
 import MovieCard from "./MovieCard";
 
 const Onshowing = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [date, setDate] = useState([]);
   const [selected, setSelected] = useState("");
-  const movies = useSelector((state)=>state.movies.filterMovies);
+  const movies = useSelector((state) => state.movies.filterMovies);
 
   useEffect(() => {
     const today = new Date();
@@ -28,7 +28,7 @@ const Onshowing = () => {
       } else if (i === 1) {
         day = "Tomorrow";
       } else {
-       day=  d.toLocaleDateString("en-US", { month: "short", day: "numeric" });
+        day = d.toLocaleDateString("en-US", { month: "short", day: "numeric" });
       }
       dDate.push({ day, value: d.toISOString().split("T")[0] });
 
@@ -36,22 +36,18 @@ const Onshowing = () => {
     }
     setDate(dDate);
     // setSelected(dDate[0].value);
-    const fetchMovies = async()=>{
-      try{
+    const fetchMovies = async () => {
+      try {
         const res = await axios.get("http://localhost:3000/movies");
-        dispatch(setMovies(res.data))
-        dispatch(setSelectDate(dDate[0].value))
-        setSelected(dDate[0].value)
+        dispatch(setMovies(res.data));
+        dispatch(setSelectDate(dDate[0].value));
+        setSelected(dDate[0].value);
         console.log(res.data);
-        
-
-      }
-      catch(err){
-           console.error(err);
-
+      } catch (err) {
+        console.error(err);
       }
     };
-    fetchMovies()
+    fetchMovies();
   }, [dispatch]);
 
   return (
@@ -60,34 +56,40 @@ const Onshowing = () => {
         Now Showing
       </h1>
       <div className="w-full p-2">
-        <div className= "  md:mx-2 flex justify-center  gap-3 mt-5 flex-wrap ">
-        {date.map((d)=>(
-          <button key={d.value} className={`bg-red px-4 md:py-1.5 md:px-6 mb-3 cursor-pointer ${
-          selected === d.value ? "bg-red-800 text-white rounded-lg"
-                : "border border-white text-white rounded-lg" }`}
-              onClick={()=>{dispatch(setSelectDate(d.value))
-                setSelected(d.value)
+        <div className="  md:mx-2 flex justify-center  gap-3 mt-5 flex-wrap ">
+          {date.map((d) => (
+            <button
+              key={d.value}
+              className={`bg-red px-4 md:py-1.5 md:px-6 mb-3 cursor-pointer ${
+                selected === d.value
+                  ? "bg-red-800 text-white rounded-lg"
+                  : "border border-white text-white rounded-lg"
+              }`}
+              onClick={() => {
+                dispatch(setSelectDate(d.value));
+                setSelected(d.value);
               }}
-              
-                >
-            {d.day}
-            
-          </button>
-        ))}
-      </div>
+            >
+              {d.day}
+            </button>
+          ))}
+        </div>
       </div>
       {/* Movie list */}
 
       <div className="w-full grid grid-cols-2 md:grid-cols-3  lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center bg-white py-2">
-        {movies.length>0 ?(
-          movies.map((m)=>(
-           <div>
-            <MovieCard movies={m} key={m.id} />
-           </div>
+        {movies.length > 0 ? (
+          movies.map((m) => (
+            <div>
+              <MovieCard movies={m} key={m.id} />
+            </div>
           ))
-        ):(
-
-          <p className="text-white">No Movies Available</p>
+        ) : (
+          <div className="col-span-full  grid place-items-center">
+            <p className="text-black text-lg font-semibold">
+              No Movies Available
+            </p>
+          </div>
         )}
       </div>
     </>
