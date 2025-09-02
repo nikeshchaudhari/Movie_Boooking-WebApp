08622a8127493b56f84239dbc35aa600567d39b8
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-02 17:50:42 +0545

    frontend

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({movies}) => {
   return (
-    <div>MovieCard</div>
+    <>
+    <div className=' md:w-[300px]   '>
+     <div className='flex flex-col '>
+      <div className='rounded-lg'>
+       <img src={movies.poster} alt={movies.title} className='h-[480px]'/>
+     </div>
+       </div>
+      <div className=''>
+      <h2 className="mt-2 font-semibold text-lg">{movies.title}</h2>
+      <p className="text-sm">{movies.genre}</p>
+      <p className="text-sm">🎬 {movies.date}</p>
+      </div>
+   
+    </div>
+    </>
   )
 }
 
