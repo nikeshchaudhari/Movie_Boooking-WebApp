39b6ca4436e577f87653c40b4fd3c670c087ecac
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-03 23:10:43 +0545

    frontend

--- src/feature/movie/movieSlice.js
+++ src/feature/movie/movieSlice.js
@@ -1,23 +1,37 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState ={
-    selectMovie :null,
-    filterMovies:[],
+const initialState = {
+  selectMovie: null,
+  filterMovies: [],
+  selectDate: "",
+  movies: [],
 };
 
+const movieSlice = createSlice({
+  name: "movies",
+  initialState,
+  reducers: {
+    setSelectMovie: (state, action) => {
+      state.selectMovie = action.payload;
+    },
+    setSelectDate: (state, action) => {
+      state.selectDate = action.payload;
+      state.filterMovies = state.movies.filter((m) => {
+          // console.log("Movie:", m.title, "ShowDate:", m.showDate);
 
-const movieSlice =createSlice({
-    name:"movies",
-    initialState,
-    reducers:{
-        setSelectMovie:(state,action)=>{
-            state.selectMovie = action.payload;
-        },
-        setFilterMovies:(state,action)=>{
-            state.filterMovies = action.payload;
-        }
-    }
-})
+        return m.showDate === action.payload;
+      });
+      // console.log("Filtered movies:", state.filterMovies);
+    },
+    setMovies: (state, action) => {
+      state.movies = action.payload;
+    },
+    setFilterMovies: (state, action) => {
+      state.filterMovies = action.payload;
+    },
+  },
+});
 
-export const{setSelectMovie,setFilterMovies}=movieSlice.actions
+export const { setSelectMovie, setSelectDate, setMovies, setFilterMovies } =
+  movieSlice.actions;
 export default movieSlice.reducer;
