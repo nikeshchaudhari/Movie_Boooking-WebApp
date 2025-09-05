f1f1f99db4a209ff9f1644d9dbd824c5ceedb86b
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-05 23:06:56 +0545

    frontend

--- src/Pages/MovieDetail.jsx
+++ src/Pages/MovieDetail.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import Navbar from "../components/Navbar";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ShowTime from "../components/ShowTime";
 
 const MovieDetail = () => {
   const { id } = useParams();
@@ -33,18 +34,34 @@ const MovieDetail = () => {
         className="w-full bg-cover bg-center h-[200px] md:h-[400px] relative overflow-hidden"
         style={{ backgroundImage: `url(${movie.bg})` }}
       >
-        <div className="absolute inset-0  h-full bg-black/60"></div>
-        <div className="flex gap-5 mx-40">
-          <div className="w-60  mt-5 ">
+        <div className="absolute inset-0 h-full bg-black/80"></div>
+        <div className="flex  gap-5 mx-40">
+          <div className="w-60  mt-5 filter brightness-100 ">
             <img src={movie.poster} alt={movie.title} />
           </div>
-          <div className="mt-10 ">
-            <h1 className="text-3xl font-bold mb-2">{movie.title}</h1>
-            <p>{movie.genre}</p>
-            <p>{movie.showDate}</p>
+          <div>
+            <div className="mt-10">
+             <h1 className="text-3xl font-bold mb-2 text-white filter brightness-100">{movie.title}</h1>
+          </div>
+          <div className="flex gap-5 mb-3">
+            <div className="bg-black/70 p-1 text-center rounded-3xl w-20 text-white filter brightness-100 ">
+                <h3 className="mx-auto">{movie.quality}</h3>
+            </div>
+            <div className="bg-black/70 p-1 text-center rounded-3xl w-20 text-white filter brightness-100 ">
+                <h3 className="mx-auto">{movie.language }</h3>
+            </div>
+            <div className="bg-red-600 p-1 text-center rounded-3xl w-30 text-white filter brightness-100 cursor-pointer ">
+                <h3 className="mx-auto">play Trailer</h3>
+            </div>
+          </div>
+          <div className=" ">
+            <p className="text-white filter brightness-100">{movie.genre}</p>
+            <p className="text-white filter brightness-100">{movie.showDate}</p>
+          </div>
           </div>
         </div>
       </div>
+      <ShowTime/>
     </>
   );
 };
