48427a9c71e781d54a7bc2e99ad6170ea0776c0b
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-06 22:01:19 +0545

    frontend

--- src/components/ShowTime.jsx
+++ src/components/ShowTime.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const ShowTime = () => {
   return (
-
     <>
-    <div>
-        
-    </div>
+    <section className="bg-[##fff]/50 mt-5">
+        <div>
+            <div>
+                <span></span>
+            </div>
+        </div>
+        <div className="w-[90vw] h-40 shadow-2xl mx-auto rounded">
+            <h1 className="mt-10 text-[40px]">Show Time</h1>
+        </div>
+    </section>
     </>
-  )
-}
+  );
+};
 
-export default ShowTime
+export default ShowTime;
