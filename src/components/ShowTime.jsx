5ce9e8f2f13618161fc31f2cfcd866ea02da4799
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-07 00:34:50 +0545

    frontend

--- src/components/ShowTime.jsx
+++ src/components/ShowTime.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 const ShowTime = () => {
   return (
     <>
     <section className="bg-[##fff]/50 mt-5">
         <div>
-            <div>
-                <span></span>
-            </div>
-        </div>
-        <div className="w-[90vw] h-40 shadow-2xl mx-auto rounded">
-            <h1 className="mt-10 text-[40px]">Show Time</h1>
+          <ul>
+            <li><span className="bg-amber-500 w-5 h-5"></span>Available</li>
+            <li><span></span>Booked</li>
+            <li><span></span>Sold Out</li>
+            <li><span></span>Not Available</li>
+          </ul>
         </div>
     </section>
     </>
