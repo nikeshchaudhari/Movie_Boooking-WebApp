cd88547e01b004a5aa4d10323a76f7b3adec90b2
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-07 22:55:34 +0545

    frontend

--- src/components/ShowTime.jsx
+++ src/components/ShowTime.jsx
@@ -1,76 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
 
 const ShowTime = () => {
   return (
     <>
-      <section className="bg-[##fff]/50 mt-10">
-        <div>
-          <div className="flex justify-center items-center">
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#00b5a1"
-                className="size-10"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-            <h2 className="mr-5">Available</h2>
-             <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#f49836"
-                className="size-10"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-            <h2 className="mr-5">Booked</h2>
-             <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#e71515"
-                className="size-10"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-            <h2 className="mr-5">Sold Out</h2>
-             <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#F8F8F8"
-                className="size-10"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-            <h2 className="mr-5">Not Available</h2>
+      <div className="">
+        <section className="bg-[##fff]/50  ">
+          <div>
+            <div className="flex justify-center items-center flex-wrap mt-10">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#00b5a1"
+                  className="size-10"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              <h2 className="mr-5">Available</h2>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#f49836"
+                  className="size-10"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              <h2 className="mr-5">Booked</h2>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#e71515"
+                  className="size-10"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              <h2 className="mr-5">Sold Out</h2>
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="#F8F8F8"
+                  className="size-10"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              <h2 className="mr-5">Not Available</h2>
+            </div>
+          </div>
+        </section>
+        <div className=" flex justify-center p-2 md:p-0">
+          <div
+            className="w-full md:w-[85vw] mt-8"
+            style={{ boxShadow: " 2px 2px 25px 2px #CFCFCF" }}
+          >
+            <div className="flex justify-between mt-5  w-[85vw] rounded-lg p-5 ">
+              <h1 className="text-[30px] font-medium">Show Times</h1>
+              <div>
+                <NavLink>Today</NavLink>
+                <NavLink>Tommarow</NavLink>
+              </div>
+            </div>
+            <div className="bg-gray-100  md:mx-5">
+              <h1 className="bg-[#D9A250]  w-full md:w-[200px] md:p-2 text-white font-medium">MOVIES CINEMA</h1>
+            </div>
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
