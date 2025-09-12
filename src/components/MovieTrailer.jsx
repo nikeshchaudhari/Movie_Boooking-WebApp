ad8a68522f16eeb9ff2e36a19b370a3e9731d16b
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-13 00:29:11 +0545

    frontend

--- src/components/MovieTrailer.jsx
+++ src/components/MovieTrailer.jsx
@@ -3,7 +3,8 @@ import ReactPlayer from "react-player";
 const MovieTrailer = ({ url }) => {
   return (
     <>
-      <div className="w-full h-[500px] bg-black  flex items-center justify-center">
+    <div className="w-full h-[500px] relative">
+         <div className="absolute inset-0 bg-black/20 z-0">
           <ReactPlayer
           src="https://youtu.be/x2QZl1dqWbY"
           playing
@@ -11,14 +12,12 @@ const MovieTrailer = ({ url }) => {
           controls
           width="60%"
           height="400px"
+           className="absolute top-0 left-0 z-10"
         />
       </div>
-      
 
-      {/* overlay */}
-      <div className="absolute top-0 w-screen h-screen bg-black">
-
-      </div>
+    </div>
+     
     </>
   );
 };
