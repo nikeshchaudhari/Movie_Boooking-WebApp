ebb6fb1bfd470925f0cf24e60bceb9ffefa10eb9
nikesh chaudhari <info.nikesh12@gmail.com>
2025-09-23 00:23:56 +0545

    movie  booking

---
+++ src/Pages/SeatBook.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+
+const SeatBook = () => {
+  const movie = useSelector((state) => state.movies.selectMovie);
+  return (
+    <>
+      <Navbar />
+      <div>
+        <div className="bg-black text-white h-[150px] w-full">
+          {movie ? (
+            <>
+              <div className="mx-5 md:mx-20 py-5">
+                <h1 className="text-[30px] font-bold">{movie.title}</h1>
+                <h1>{movie.genre}</h1>
+                <span>{movie.showDate}</span>
+              </div>
+            </>
+          ) : (
+            <p>No movie selected</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SeatBook;
